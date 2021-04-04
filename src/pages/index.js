cbc9a13b8cbd144eb8d68fb43ca5750344a2bc57
Gabby Pelton <gabriellapelton@gmail.com>
2021-04-04 12:43:42 -0500

    replace blog with shortBio on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,28 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import { Helmet } from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import { Helmet } from "react-helmet";
+import Hero from "../components/hero";
+import Layout from "../components/layout";
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const [author] = get(this, "props.data.allContentfulPerson.edges");
 
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: "#eee" }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <div className="wrapper">{author.node.shortBio.shortBio}</div>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default RootIndex
+export default RootIndex;
 
 export const pageQuery = graphql`
   query HomeQuery {
@@ -74,7 +61,7 @@ export const pageQuery = graphql`
               maxWidth: 1180
               maxHeight: 480
               resizingBehavior: PAD
-              background: "rgb:000000"
+              background: "rgb:eeeeee"
             ) {
               ...GatsbyContentfulFluid_tracedSVG
             }
@@ -83,4 +70,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
